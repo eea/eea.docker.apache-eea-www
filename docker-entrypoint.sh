3905271ffa66f8823c431bd9520257b99c910b74
Alexandru Ghica <alecghica@gmail.com>
2018-04-19 15:16:12 +0000

    load module later on to have all dependencies available, refs #88414

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -55,8 +55,8 @@ chown -R www-data:www-data /var/www/html/
 #
 # BROTLI module
 #
-if [ -z "$(grep /usr/local/apache2/conf/httpd.conf -e brotli_module)" ]; then
-  sed -i 's|# LoadModule foo_module modules/mod_foo.so|# LoadModule foo_module modules/mod_foo.so\nLoadModule brotli_module modules/mod_brotli.so|' /usr/local/apache2/conf/httpd.conf
+if [ ! -z "$(grep /usr/local/apache2/conf/httpd.conf -e brotli_module)" ]; then
+  sed -i 's|LoadModule rewrite_module modules/mod_rewrite.so|LoadModule rewrite_module modules/mod_rewrite.so\nLoadModule brotli_module modules/mod_brotli.so|' /usr/local/apache2/conf/httpd.conf
 fi
 
 #
