df58d4f87fefd4a78d1ac5da6d4b3505c531f9b9
Alexandru Ghica <alecghica@gmail.com>
2018-04-19 11:30:22 +0000

    properlly load brotli module, refs #88414

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -53,12 +53,12 @@ chown -R www-data:www-data /var/local/www-logs/eea/
 chown -R www-data:www-data /var/www/html/
 
 #
-# TRACEVIEW
+# TRACEVIEW & BROTLI modules
 #
 if [ ! -z "$TRACEVIEW" ]; then
     TRACEVIEW_CONFIGURED=`cat /usr/local/apache2/conf/httpd.conf | grep oboe`
     if [ -z "$TRACEVIEW_CONFIGURED" ]; then
-        sed -i 's|# LoadModule foo_module modules/mod_foo.so|LoadModule oboe_module modules/mod_oboe.so\nLoadModule oboe_ps_module modules/mod_oboe_ps_ap24.so|' /usr/local/apache2/conf/httpd.conf
+        sed -i 's|# LoadModule foo_module modules/mod_foo.so|LoadModule oboe_module modules/mod_oboe.so\nLoadModule oboe_ps_module modules/mod_oboe_ps_ap24.so\nLoadModule brotli_module modules/mod_brotli.so|' /usr/local/apache2/conf/httpd.conf
         echo 'IncludeOptional conf/extra/oboe*.conf' >> /usr/local/apache2/conf/httpd.conf
         /usr/sbin/traceview-config $TRACEVIEW
     fi
