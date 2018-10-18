daf996a128435f2453400e7d6ca39b062a56d97a
valentinab25 <valentinab25>
2018-10-18 14:13:05 +0300

    Add move non-current logs to other location Refs #98892

--- src/archive_old_logs.sh
+++ src/archive_old_logs.sh
@@ -2,5 +2,18 @@
 
 DAYS_TO_KEEP=${APACHE_LOGS_DAYNR:-14}
 
-find /var/log/httpd/ -name access_log* -mtime +${DAYS_TO_KEEP} -exec rm -rf {} \;
+CURRENT_LOG=$(basename $1)
+
+LOG_LOCATION=/var/log/httpd/
+
+if [ -n "${APACHE_NFS_LOCATION}" ]; then
+        LOG_LOCATION=${APACHE_NFS_LOCATION}/$(hostname)
+        mkdir -p $LOG_LOCATION
+	#move old logs to NFS volume
+        find /var/log/httpd/ -name access_log* ! -name $CURRENT_LOG -exec mv {} $LOG_LOCATION \;
+fi
+
+
+#delete old logs
+find $LOG_LOCATION -name access_log* -mtime +${DAYS_TO_KEEP} -exec rm -rf {} \;
 
